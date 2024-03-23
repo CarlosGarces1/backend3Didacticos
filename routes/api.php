97eabfe4da9cac00292ebe7d04003ae42646cec3
Carlos Felipe Garces Yepes <64514142+CarlosGarces1@users.noreply.github.com>
2024-03-23 00:44:05 -0500

    Actualizacion

--- routes/api.php
+++ routes/api.php
@@ -13,18 +13,18 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// Route::group(['middleware' => ['auth:sanctum']], function () {
+Route::group(['middleware' => ['auth:sanctum']], function () {
 
-Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
 
-Route::get('/posts', [PostsController::class, 'index']);
-Route::post('/posts', [PostsController::class, 'store']);
-Route::put('/posts/{id}', [PostsController::class, 'update']);
-Route::delete('/posts/{id}', [PostsController::class, 'destroy']);
+    Route::get('/posts', [PostsController::class, 'index']);
+    Route::post('/posts', [PostsController::class, 'store']);
+    Route::put('/posts/{id}', [PostsController::class, 'update']);
+    Route::delete('/posts/{id}', [PostsController::class, 'destroy']);
 
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
-// });
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+});
